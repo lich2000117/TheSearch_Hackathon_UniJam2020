210b0ad65214214aa41d3b78f2fa5ed3b5b3b2c5
Chenghao Li <lich2000117@gmail.com>
2020-09-15 12:00:15 +0800

    updated shader for floor
    NPC follows:
    https://sharpcoderblog.com/blog/npc-follow-player-in-unity-3d

---
+++ Assets/Scripts/NPCFollow.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCFollow : MonoBehaviour
+{
+    public Transform freind;
+    private int movespeed;
+
+    public int speed;
+    public Rigidbody rb;
+    // Use this for initialization
+    void Start()
+    {
+        speedup();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        transform.LookAt(freind);
+        Vector3 eulerAngles = transform.rotation.eulerAngles;
+        eulerAngles.x = 0;
+        eulerAngles.z = 0;
+        transform.rotation = Quaternion.Euler(eulerAngles);
+
+        rb.AddRelativeForce(0, 0, movespeed);
+
+    }
+
+    public void OnTriggerStay(Collider other)
+    {
+
+
+        if (other.tag == "Player")
+        {
+            movespeed = 0;
+        }
+
+    }
+    public void OnTriggerExit(Collider other)
+    {
+        speedup();
+    }
+
+    public void speedup()
+    {
+        movespeed = speed;
+    }
+}
